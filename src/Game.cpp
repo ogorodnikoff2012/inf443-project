a92b4fb9c86ab3849b36bc2f640a98bb20142a24
Vladimir Ogorodnikov <volodya5000@gmail.com>
2021-05-31 18:31:41 +0200

    Small clean up

--- src/Game.cpp
+++ src/Game.cpp
@@ -173,7 +173,7 @@ void Game::RenderScene() {
 }
 
 void Game::RenderGameScene() {
-  glClearColor(0.7f, 1.0f, 1.0f, 1.0f);
+  glClearColor(0.4f, 1.0f, 1.0f, 1.0f);
   glClear(GL_COLOR_BUFFER_BIT);
   glClear(GL_DEPTH_BUFFER_BIT);
 
